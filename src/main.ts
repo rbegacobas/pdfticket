8a7ec562ec37cf05816bf2b257d5c613037952e9
Ramon Bega Cobas <rbegacobas@gmail.com>
2024-09-04 22:53:26 -0300

    Fix port binding for Heroku

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  //await app.listen(3000);
+  // Heroku proporciona el puerto en la variable de entorno PORT
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
